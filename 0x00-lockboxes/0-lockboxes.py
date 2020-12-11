f88fe5e847a90948fcbf8ee42f05b5c9e6914a2d
mohamedalisouissi25 <dali-madridist@hotmail.fr>
2020-12-11 14:11:59 +0000

    debug

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -2,16 +2,15 @@
 """[boxes]"""
 
 
-
 def canUnlockAll(boxes):
 
     lock_boxes = [0]
     for box in range(len(boxes)):
         for key in boxes[box]:
-            if key not in lock_boxes
-                if key != box
+            if key not in lock_boxes:
+                if key != box:
                     if key < len(boxes):
-                    lock_boxes.append(key)
+                        lock_boxes.append(key)
     if len(lock_boxes) == len(boxes):
         return True
     return False
