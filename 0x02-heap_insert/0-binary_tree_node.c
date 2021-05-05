190ea500a256a07be7719a35a9b0fcf9d5cdbf0b
mohamedalisouissi25 <dali-madridist@hotmail.fr>
2021-05-05 14:58:05 +0100

    commit

--- 0x02-heap_insert/0-binary_tree_node.c
+++ 0x02-heap_insert/0-binary_tree_node.c
@@ -11,6 +11,7 @@
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *newnode;
+
 	newnode = malloc(sizeof(binary_tree_t));
 	if (newnode == NULL)
 		return (NULL);
