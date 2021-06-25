a503cff6b825d2ffcfddf4bf08f52d2b3a681585
mohamedalisouissi25 <dali-madridist@hotmail.fr>
2021-06-25 14:03:13 +0100

    commit

---
+++ 0x05-linked_list_palindrome/0-is_palindrome.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+
+/**
+ * is_palindrome - function in C that checks if a singly linked list is a palindrome.
+ * @head: input
+ * Return: output
+ */
+
+int is_palindrome(listint_t **head)
+{
+	int i, x = 0;
+	int y;
+	int tab[2069];
+
+	if (*head == NULL)
+	return (1);
+
+	while (*head)
+	{
+		tab[x] = (*head)->n;
+		*head = (*head)->next;
+		x = x + 1;
+	}
+	--x;
+	y = x / 2;
+
+	for (i = 0; i < y; i++)
+	{
+		if (tab[i] != tab[x])
+		return (0);
+		++i;
+		--x;
+	}
+
+	return (1);
+}
